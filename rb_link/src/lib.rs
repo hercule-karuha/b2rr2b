b30ad1f5079925b22dc97d2e189583f4473d294d
karuha <aa81251199@outlook.com>
2024-05-16 17:03:27 +0800

    reduce nest

--- rb_link/src/lib.rs
+++ rb_link/src/lib.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, VecDeque};
 
 use std::io::{Read, Write};
-use std::os::unix::net::UnixListener;
+use std::os::unix::net::{UnixListener, UnixStream};
 use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
 use std::time::Duration;
@@ -68,52 +68,38 @@ impl B2RServer {
         thread::spawn(move || {
             let listener = UnixListener::bind("/tmp/b2rr2b").expect("Failed to bind Unix listener");
             for stream in listener.incoming() {
-                match stream {
-                    Ok(mut stream) => {
-                        println!("connect comeing");
-                        let mut sz_buf: Vec<u8> = vec![0; 4];
-                        stream
-                            .read_exact(&mut sz_buf)
-                            .expect("Failed to read from stream");
-                        let sz_msg: usize = get_msg_size(sz_buf) as usize;
-                        let mut buffer = vec![0; sz_msg];
-                        let _ = stream
-                            .read_exact(&mut buffer)
-                            .expect("Failed to read from stream");
-                        println!("sz_msg is {}", sz_msg);
-                        if let Ok(message) = bincode::deserialize::<GetPutMessage>(&buffer) {
-                            match message {
-                                GetPutMessage::Get(id) => {
-                                    println!("receive get from id {}", id);
-                                    loop {
-                                        let mut r2b_cache = r2b_cache.lock().unwrap();
-                                        if let Some(queue) = r2b_cache.get_mut(&id) {
-                                            if let Some(r2b_message) = queue.pop_front() {
-                                                stream.write_all(&r2b_message.message).unwrap();
-                                                drop(r2b_cache);
-                                                break;
-                                            }
-                                        }
-                                        drop(r2b_cache);
-                                        thread::sleep(Duration::from_millis(100));
-                                    }
-                                }
-                                GetPutMessage::Put(b2r_message) => {
-                                    println!("receive put to id {}", b2r_message.id);
-                                    let mut b2r_cache = b2r_cache.lock().unwrap();
-                                    let queue = b2r_cache
-                                        .entry(b2r_message.id)
-                                        .or_insert_with(VecDeque::new);
-                                    queue.push_back(b2r_message);
-                                    println!("put end");
+                let mut stream = match stream {
+                    Ok(stream) => stream,
+                    Err(_) => todo!(),
+                };
+                let message = match receive_getput(&mut stream) {
+                    Ok(m) => m,
+                    Err(_) => todo!(),
+                };
+                match message {
+                    GetPutMessage::Get(id) => {
+                        println!("receive get from id {}", id);
+                        loop {
+                            let mut r2b_cache = r2b_cache.lock().unwrap();
+                            if let Some(queue) = r2b_cache.get_mut(&id) {
+                                if let Some(r2b_message) = queue.pop_front() {
+                                    stream.write_all(&r2b_message.message).unwrap();
+                                    drop(r2b_cache);
+                                    break;
                                 }
                             }
-                        } else {
-                            eprintln!("Failed to deserialize GetPutMessage");
+                            drop(r2b_cache);
+                            thread::sleep(Duration::from_millis(100));
                         }
                     }
-                    Err(err) => {
-                        eprintln!("Error accepting connection: {}", err);
+                    GetPutMessage::Put(b2r_message) => {
+                        println!("receive put to id {}", b2r_message.id);
+                        let mut b2r_cache = b2r_cache.lock().unwrap();
+                        let queue = b2r_cache
+                            .entry(b2r_message.id)
+                            .or_insert_with(VecDeque::new);
+                        queue.push_back(b2r_message);
+                        println!("put end");
                     }
                 }
             }
@@ -150,4 +136,19 @@ impl B2RServer {
 
 fn get_msg_size(bytes: Vec<u8>) -> u32 {
     u32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]])
+}
+
+fn receive_getput(stream: &mut UnixStream) -> Result<GetPutMessage, Box<bincode::ErrorKind>> {
+    println!("connect comeing");
+    let mut sz_buf: Vec<u8> = vec![0; 4];
+    stream
+        .read_exact(&mut sz_buf)
+        .expect("Failed to read from stream");
+    let sz_msg: usize = get_msg_size(sz_buf) as usize;
+    let mut buffer = vec![0; sz_msg];
+    let _ = stream
+        .read_exact(&mut buffer)
+        .expect("Failed to read from stream");
+    println!("sz_msg is {}", sz_msg);
+    return bincode::deserialize::<GetPutMessage>(&buffer);
 }
